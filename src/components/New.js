a9c9a8e225040a8a89d7643e0f90ab8c822a181c
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-15 01:05:11 +0530

    submitting code-Thu Jun 15 2023 01:05:10 GMT+0530 (India Standard Time)

--- src/components/New.js
+++ src/components/New.js
@@ -3,10 +3,10 @@ import React from "react";
 
 const New = ({ value, current }) => {
   return (
-    current ? (<div onClick={(e)=>{e.stopPropagation()}}>
+    current ? (<div onClick={value}>
       <div className="modal-overlay">
         <button className="modal-close" onClick={value}>Close</button>
-        <p>This is modal content</p>
+        <p>This is the content of the modal.</p>
       </div></div>
     ) : null
   );
