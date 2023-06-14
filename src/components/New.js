c1561b7c5d9f3054f7d0e7ca1a92bb0ae92dc4be
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-15 00:20:04 +0530

    submitting code-Thu Jun 15 2023 00:20:04 GMT+0530 (India Standard Time)

--- src/components/New.js
+++ src/components/New.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const New=({value,current})=>{
 return(current?(<div className="model-overlay"><button className="model-close" onClick={value}>Close</button>
-<p>This is modal content</p></div>):null)}
+<p>This is content modal </p></div>):null)}
 
 export default New;
 // import React from "react";
